92553796e47bced9afa69e2dea44854bfcc44077
rommuell <rommuell@student.ethz.ch>
2017-04-21 17:19:57 +0200

    loop egm

---
+++ night_shift.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#! -*- encoding: utf-8 -*-
+
+# modified Roman Mueller, 02/2017
+import subprocess
+
+command = "/home/rm/code/egm/build/main -i /home/rm/Documents/master_thesis/data/vicon_leo/bag1/okvis_output -c /home/rm/Documents/master_thesis/data/vicon_leo/bag1/config_visensor_mono.yaml -g /home/rm/Documents/master_thesis/data/vicon_leo/bag1/vicon_data_reordered.csv"
+
+command2 = ["/home/rm/code/egm/build/main",
+            "-i", "/home/rm/Documents/master_thesis/data/vicon_leo/bag1/okvis_output",
+            "-c", "/home/rm/Documents/master_thesis/data/vicon_leo/bag1/config_visensor_mono.yaml",
+            "-g", "/home/rm/Documents/master_thesis/data/vicon_leo/bag1/vicon_data_reordered.csv"]
+
+while 1:
+    pFeatures = subprocess.Popen(command2)
+    pFeatures.wait()
